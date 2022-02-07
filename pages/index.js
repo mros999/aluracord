80060e29accf28e37047439a1e1eb758a6fe5c8c
Marquinho Filho <marcofilho978@gmail.com>
2022-02-07 09:31:06 -0300

    aluracord

---
+++ pages/index.js
@@ -0,0 +1,142 @@
+import {Box, Button, Text, TextField, Image} from '@skynexui/components';
+import appConfig from '../config.json';
+import React from 'react';
+import {useRouter} from 'next/router';
+
+
+
+function Title(props) {
+    const Tag = props.tag;
+    return (
+       <>
+        <Tag>{props.children}</Tag>
+
+        <style jsx>{`
+        ${Tag}{
+            color: ${appConfig.theme.colors.neutrals['99']}; 
+            font-size: 24px;
+            font-weight: 600;
+        }
+        `}</style>
+
+    </>
+    )
+}
+export default function PaginaInicial() {
+    const [username, setUsername] = React.useState('mros999');
+    const roteamento = useRouter();
+    return (
+      <>
+        <Box
+          styleSheet={{
+            display: 'flex', alignItems: 'center', justifyContent: 'center',
+            backgroundPosition: 'center',
+            backgroundImage: 'url(https://images8.alphacoders.com/998/998795.jpg)',
+            backgroundRepeat: 'no-repeat', backgroundSize: 'cover', backgroundBlendMode: 'multiply',
+          }}
+        >
+          <Box
+            styleSheet={{
+              display: 'flex',
+              alignItems: 'center',
+              justifyContent: 'space-between',
+              flexDirection: {
+                xs: 'column',
+                sm: 'row',
+              },
+              width: '100%', maxWidth: '700px',
+              borderRadius: '5px', padding: '32px', margin: '16px',
+              boxShadow: '0 2px 10px 0 rgb(0 0 0 / 20%)',
+              backgroundColor: appConfig.theme.colors.neutrals[700],
+            }}
+          >
+            {/* Formulário */}
+            <Box
+              as="form"
+              onSubmit={function (infosDoEvento){
+                infosDoEvento.preventDefault();
+                roteamento.push(`/chat?username=${username}`);
+              }}
+              styleSheet={{
+                display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center',
+                width: { xs: '100%', sm: '50%' }, textAlign: 'center', marginBottom: '32px',
+              }}
+            >
+              <Title tag="h2">Boas vindas de volta!</Title>
+              <Text variant="body3" styleSheet={{ marginBottom: '32px', color: appConfig.theme.colors.neutrals[300] }}>
+                {appConfig.name}
+              </Text>
+  
+              <TextField 
+              value={username}
+              onChange={function (event){
+                console.log('usuario digitou', event.target.value);
+                const valor = event.target.value;
+                setUsername(valor);
+              }}
+                fullWidth
+                textFieldColors={{
+                  neutral: {
+                    textColor: appConfig.theme.colors.neutrals[200],
+                    mainColor: appConfig.theme.colors.neutrals[900],
+                    mainColorHighlight: appConfig.theme.colors.primary[500],
+                    backgroundColor: appConfig.theme.colors.neutrals[800],
+                  },
+                }}
+              />
+              <Button
+                type='submit'
+                label='Entrar'
+                fullWidth
+                buttonColors={{
+                  contrastColor: appConfig.theme.colors.neutrals["99"],
+                  mainColor: appConfig.theme.colors.neutrals[500],
+                  mainColorLight: appConfig.theme.colors.neutrals[900],
+                  mainColorStrong: appConfig.theme.colors.neutrals[900],
+                }}
+              />
+            </Box>
+            {/* Formulário */}
+  
+  
+            {/* Photo Area */}
+            <Box
+              styleSheet={{
+                display: 'flex',
+                flexDirection: 'column',
+                alignItems: 'center',
+                maxWidth: '200px',
+                padding: '16px',
+                backgroundColor: appConfig.theme.colors.neutrals[700],
+                border: '1px solid',
+                borderColor: appConfig.theme.colors.neutrals[999],
+                borderRadius: '10px',
+                flex: 1,
+                minHeight: '240px',
+              }}
+            >
+              <Image
+                styleSheet={{
+                  borderRadius: '50%',
+                  marginBottom: '16px',
+                }}
+                src={`https://github.com/${username}.png`}
+              />
+              <Text
+                variant="body4"
+                styleSheet={{
+                  color: appConfig.theme.colors.neutrals[200],
+                  backgroundColor: appConfig.theme.colors.neutrals[900],
+                  padding: '3px 10px',
+                  borderRadius: '1000px'
+                }}
+              >
+                {username}
+              </Text>
+            </Box>
+            {/* Photo Area */}
+          </Box>
+        </Box>
+      </>
+    );
+  }
